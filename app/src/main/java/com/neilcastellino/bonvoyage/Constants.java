e8fcf7c7096794bc9d926004e4d0cab7a0505ca7
Neil Castellino <neilcastellino@gmail.com>
2018-01-08 00:22:13 +0530

    Create Constants.java

---
+++ app/src/main/java/com/neilcastellino/bonvoyage/Constants.java
@@ -0,0 +1,7 @@
+package com.neilcastellino.bonvoyage;
+
+public class Constants {
+    public static final String FIREBASE_URL = "FIREBASE_URL";
+    public static final String username = "EMAIL_ID";
+    public static final String password = "PASSWORD";
+}
